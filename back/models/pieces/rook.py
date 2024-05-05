5c8486badd520370ca4d2b322075e4aded3b6f52
Ismail Naziz <ismailnaziz2@gmail.com>
2024-05-05 15:15:32 +0200

    Implementing rook without tests

--- back/models/pieces/rook.py
+++ back/models/pieces/rook.py
@@ -19,12 +19,8 @@ class Rook(Piece):
     @property
     def possible_positions(self):
         # corresponds to natural piece movement if nothing interferes
-        return []
-
-    @property
-    def possible_capture_positions(self):
-        # corresponds to positions that are only reachable when capturing other pieces
-        return []
+        return [(possible_row, self.col) for possible_row in range(BOARD_SIZE) if possible_row != self.row] + \
+               [(self.row, possible_col) for possible_col in range(BOARD_SIZE) if possible_col != self.col]
 
     def update_allowed_positions(self, chess_model):
         """
@@ -33,6 +29,18 @@ class Rook(Piece):
         :param col: col of the piece in the board
         modifies self.allowed_positions
         """
+        if chess_model.player_turn != self.player:
+            self.allowed_positions = []
+            return
 
-        self.allowed_positions = [(r, c) for r in range(BOARD_SIZE) for c in range(BOARD_SIZE)]
+        in_bound_positions = [position for position in self.possible_positions if is_in_bound(*position)]
 
+        in_sight_positions = in_bound_positions
+        for piece in chess_model.pieces:
+            if piece != self and (piece.row, piece.col) in in_sight_positions:
+                straight_line_filter_function = self.get_straight_line_filter_function(piece.row, piece.col)
+                in_sight_positions = [position for position in in_sight_positions if straight_line_filter_function(*position)]
+                if piece.player != self.player:
+                    in_sight_positions.append((piece.row, piece.col))
+
+        self.allowed_positions = in_sight_positions
